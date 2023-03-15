e1d105ce6c81021712aebdc3b9d00255e2ebbbc1
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-03-15 19:15:57 +0100

    main elements styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 
-
+//make animation (rolling of big words) that plays with delay after you typed something (if something was typed earlier the animation does not occur and timer is resent)
+//delete text in search button
+//switching between the types of words and coloring the buttons
+//Nederlands wordenboek switches between languages or displays word in Dutch and English
+//maybe add some floating gradient to the word Nederlands (check codepen in email for the ideas or just google)
+//when scroll reaches certain position position of the searchbar becomes fixed
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,16 +13,45 @@ function App() {
     <div className="App">
       <div className="title-and-search">
         <div className="search-wrapper">
-          <input type="text" placeholder='Search' />
+          <input type="text" placeholder='Zoeken...'/>
         </div>
         <h1 className='dictionary-title'>Nederlands woordenboek</h1>
       </div>
 
       <div className="choice-panel">
         <button className="all">All</button>
-        <button className="words">Words</button>
+        <button className="nouns">Nouns</button>
+        <button className="verbs">Verbs</button>
+        <button className="adjectives">Adjectives</button>
         <button className="sentences">Sentences</button>
       </div>
+
+      <div className="words">
+        <p className='word'>Belangrijk - important</p>
+        <p className='word'>Verschillende - different</p>
+        <p className='word'>Zoeken - to search</p>
+
+
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+        <p className='word'>Zoeken - to search</p>
+
+      </div>
+
     </div>
   )
 }
